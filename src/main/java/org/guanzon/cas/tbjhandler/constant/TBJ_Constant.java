2c88ba61758f18b8f8b496c510fc93971c106220
Jeitee <93184955+jeitee@users.noreply.github.com>
2026-03-11 13:04:08 +0800

    03112026d

--- src/main/java/org/guanzon/cas/tbjhandler/constant/TBJ_Constant.java
+++ src/main/java/org/guanzon/cas/tbjhandler/constant/TBJ_Constant.java
@@ -4,18 +4,16 @@ package org.guanzon.cas.tbjhandler.constant;
  * TBJ_Constant ----------------------------------------------------------
  * Defines constant values for Transaction Book Journal (TBJ) status codes.
  * 
- * VOID = "0"       : Transaction is VOID
- * OPEN = "1"       : Transaction has OPEN
- * CONFIRMED = "2"  : Transaction has been CONFIRMED
+ * VOID = "3"       : Transaction is VOID
+ * OPEN = "0"       : Transaction has OPEN
+ * CONFIRMED = "1"  : Transaction has been CONFIRMED
  */
 public class TBJ_Constant {
-
-    /** Transaction is VOID */
-    public static final String VOID = "0";
-
     /** Transaction is OPEN */
-    public static final String OPEN = "1";
-    
+    public static final String OPEN = "0";
     /** Transaction has been CONFIRMED */
-    public static final String CONFIRMED = "2";
+    public static final String CONFIRMED = "1";
+    /** Transaction is VOID */
+    public static final String VOID = "3";
+    
 }
